4a93fda5d94240a9cfe2fb200517c02351277554
amyrhuerta <89152622+amyrhuerta@users.noreply.github.com>
2023-06-06 17:44:09 -0600

    Update tablerofinal.py

--- tablerofinal.py
+++ tablerofinal.py
@@ -12,6 +12,8 @@ costos=pd.read_csv("https://drive.google.com/file/d/1kZOOvLyTGsIqlwiKbbhHpXvtve6
 st.image("https://calorycontrol.com.mx/staging/wp-content/uploads/2021/04/LogoHorizontal-768x210.png", use_column_width=True)
 
 st.markdown("<h1 style='text-align: center; font-weight: bold; color: black; font-size: 48px; font-family: Montserrat;'>Tablero interactivo de desempeño</h1>", unsafe_allow_html=True)
+years_to_filter = st.slider('year', 2021, 2023, 2021)
+filtered_data = facturacion[facturacion[FECHA_DOC].dt.year == years_to_filter]
 
 st.markdown("<h1 style='text-align: left; color: black; font-size: 24px; font-family: Montserrat;'>Ingresos</h1>", unsafe_allow_html=True)
 
