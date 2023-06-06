241177099214563ff3725cc5476606502eec1a43
renatachavezm22 <124286680+renatachavezm22@users.noreply.github.com>
2023-06-06 16:59:01 -0600

    Update tablerofinal.py

--- tablerofinal.py
+++ tablerofinal.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.header(':red[Calor y Control] :gear:')
+st.markdown("<h1 style='text-align: center; font-weight: bold; color: red;'>Calor y Control <span style='font-size: 1.2em;'>⚙️</span></h1>", unsafe_allow_html=True)
 st.markdown("")
