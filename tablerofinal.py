5f963afe83b173b7cb3b96710a7f95cb1163e41f
renatachavezm22 <124286680+renatachavezm22@users.noreply.github.com>
2023-06-06 17:13:52 -0600

    Update tablerofinal.py

--- tablerofinal.py
+++ tablerofinal.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.image("https://calorycontrol.com.mx/staging/wp-content/uploads/2021/04/LogoHorizontal-768x210.png", width=300)
+st.image("https://calorycontrol.com.mx/staging/wp-content/uploads/2021/04/LogoHorizontal-768x210.png", use_column_width=True)
 
-st.markdown("<h1 style='text-align: right; font-weight: bold; color: black; font-size: 36px;'>Tablero interactivo de desempeño</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; font-weight: bold; color: black; font-size: 36px;'>Tablero interactivo de desempeño</h1>", unsafe_allow_html=True)
 
