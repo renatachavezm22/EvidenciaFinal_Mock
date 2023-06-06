0a8bb3213d5d1fa02f54d1e5b913584e626ddfd2
renatachavezm22 <124286680+renatachavezm22@users.noreply.github.com>
2023-06-06 17:10:35 -0600

    Update tablerofinal.py

--- tablerofinal.py
+++ tablerofinal.py
@@ -3,4 +3,5 @@ import pandas as pd
 import numpy as np
 
 st.image("https://calorycontrol.com.mx/staging/wp-content/uploads/2021/04/LogoHorizontal-768x210.png", width=300)
-st.markdown("<h1 style='text-align: right; font-weight: bold; color: black;'> Tablero interactivo de desempeño <span style='font-size: 1.2em;'>⚙️</span></h1>", unsafe_allow_html=True)
+
+st.markdown("<h1 style='text-align: right; font-weight: bold; color: black;'> Tablero interactivo de desempeño <span style='font-size: 0.9 em;'>⚙️</span></h1>", unsafe_allow_html=True)
