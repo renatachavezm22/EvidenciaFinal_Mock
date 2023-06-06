14a0f29a42aa821265f2a05b07664b671989424d
renatachavezm22 <124286680+renatachavezm22@users.noreply.github.com>
2023-06-06 17:05:02 -0600

    Update tablerofinal.py

--- tablerofinal.py
+++ tablerofinal.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.markdown(style='text-align: center; font-weight: bold; color: black;'>Tablero de <span style='font-size: 1.2em;'>⚙️</span></h1>, unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; font-weight: bold; color: red;'>Calor y Control <span style='font-size: 1.2em;'>⚙️</span></h1>", unsafe_allow_html=True)
 st.image("https://drive.google.com/file/d/1A0pGMyRr2bLzuVw8TlbTcaatvymxQE23/view?usp=sharing", width=300, height=200)
