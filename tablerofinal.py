89d4014c5a381bf3181f467c16216ea256253e0e
renatachavezm22 <124286680+renatachavezm22@users.noreply.github.com>
2023-06-06 17:00:30 -0600

    Update tablerofinal.py

--- tablerofinal.py
+++ tablerofinal.py
@@ -2,4 +2,4 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.markdown("<h1 style='text-align: center; font-weight: bold; color: red;'>Tablero de <span style='font-size: 1.2em;'>⚙️</span></h1>", unsafe_allow_html=True)
+st.markdown(style='text-align: center; font-weight: bold; color: black;'>Tablero de <span style='font-size: 1.2em;'>⚙️</span></h1>, unsafe_allow_html=True)
